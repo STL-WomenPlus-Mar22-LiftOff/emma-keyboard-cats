5c0128d5d07c85f81e4cb07f1076ae539a0d0a16
Zombie <kroshel77@gmail.com>
2022-12-22 17:33:54 -0600

    First commit!

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Keyboard_Cats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

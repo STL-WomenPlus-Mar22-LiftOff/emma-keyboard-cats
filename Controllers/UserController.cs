cf085724abb31a4ad6dcfb5d6ab0a8d380b53ff0
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2023-01-08 22:20:13 -0600

    filling out basics

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,12 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Keyboard_Cats.Models;
+using Keyboard_Cats.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Keyboard_Cats.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
-        {
+      
+            // GET: /<controller>/
+            public IActionResult Index()
+            {
+                //List<User> users = new List<User>();
+                return View();
+            }
+
+            public IActionResult Add()
+            {
+                AddUserViewModel addUserViewModel = new AddUserViewModel();
+                return View(addUserViewModel);
+            }
+
+            [HttpPost]
+            [Route("/user")]
+            public IActionResult SubmitAddUserForm(AddUserViewModel addUserViewModel)
+            {
+                if (ModelState.IsValid)
+                {
+
+                    if (addUserViewModel.Password == addUserViewModel.VerifyPassword)
+                    {
+                        User newUser = new User
+                        {
+                            Username = addUserViewModel.Username,
+                            Email = addUserViewModel.Email,
+                            Password = addUserViewModel.Password,
+                        };
+
+                        return View("Index", newUser);
+                    }
+                    else
+                    {
+                        ViewBag.error = "Passwords do not match.";
+                        return View("Add", addUserViewModel);
+                    }
+
+
+                }
+
+
+                return View("Add", addUserViewModel);
+            }
+
             return View();
-        }
+        
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -2,5 +2,10 @@
 {
     public class User
     {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+       
     }
 }

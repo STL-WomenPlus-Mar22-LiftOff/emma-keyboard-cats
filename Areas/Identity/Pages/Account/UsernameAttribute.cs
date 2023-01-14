c8f2ebf6cd1609d0651a505a93a7b49baa828a75
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2023-01-14 15:44:32 -0600

    fix error

--- Areas/Identity/Pages/Account/UsernameAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-namespace Keyboard_Cats.Areas.Identity.Pages.Account
-{
-    internal class UsernameAttribute : Attribute
-    {
-    }
-}

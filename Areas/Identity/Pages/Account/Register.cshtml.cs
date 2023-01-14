d1803a9e5d9e68d2885d5323f97f5762845693fa
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2023-01-14 15:37:18 -0600

    adding username to Register page

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,11 @@ namespace Keyboard_Cats.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [Username]
+            [Display(Name ="Username")]
+            public string Username { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.

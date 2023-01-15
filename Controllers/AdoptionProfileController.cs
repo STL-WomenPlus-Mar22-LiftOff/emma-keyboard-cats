5653b821997dee7abf52443b91c0e19ecfa8f542
saranell <76405224+saranell@users.noreply.github.com>
2023-01-14 22:35:58 -0600

    Fixing Bootstrap styling

---
+++ Controllers/AdoptionProfileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Keyboard_Cats.Controllers
+{
+    public class AdoptionProfileController : Controller
+    {
+        [HttpGet]
+        [Route("/adoptionprofile/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

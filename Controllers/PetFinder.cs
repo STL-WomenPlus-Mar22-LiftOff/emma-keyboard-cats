091b9587a5451db6af76a96c59d7baffcaf54588
KaitlynViers <107895980+KaitlynViers@users.noreply.github.com>
2023-01-15 15:41:39 -0600

    articles moved

---
+++ Controllers/PetFinder.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Keyboard_Cats.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetFinder : ControllerBase
+    {
+        // GET: api/<PetFinder>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PetFinder>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PetFinder>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PetFinder>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PetFinder>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        
+    }
+}

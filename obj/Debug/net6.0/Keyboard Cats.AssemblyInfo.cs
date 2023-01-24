4c68238a15ed0cfc17e988cfd04344b215eb2385
saranell <76405224+saranell@users.noreply.github.com>
2023-01-23 18:17:12 -0600

    Merging things

--- obj/Debug/net6.0/Keyboard Cats.AssemblyInfo.cs
+++ obj/Debug/net6.0/Keyboard Cats.AssemblyInfo.cs
@@ -11,7 +11,6 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Keyboard Cats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]

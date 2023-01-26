51b1d3ff0213ac3ac80d8998584a089827c5dc42
KaitlynViers <107895980+KaitlynViers@users.noreply.github.com>
2023-01-25 20:28:37 -0600

    CatGallery is giving issues

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Keyboard_Cats.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Keyboard_Cats.Controllers
@@ -31,8 +32,23 @@ namespace Keyboard_Cats.Controllers
         }
 
         [Route("/CatGallery")]
-        public IActionResult CatGallery()
+        public async IAsyncEnumerable<T> CatGallery()
         {
+            string Baseurl = "api.petfinder.com/v2";
+            List<Cat> catInfo = new List<Cat>();
+
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(Baseurl);
+            httpClient.DefaultRequestHeaders.Clear();
+            HttpResponseMessage Res = await httpClient.GetAsync("animals?type=cat?location=62208");
+            if (Res.IsSuccessStatusCode)
+            {
+                var CatResponse = Res.Content.ReadAsStringAsync().Result;
+                catInfo = JsonConvert.DeserializeObject<List<Cat>>(CatResponse);
+            }
+
+            Console.WriteLine(catInfo);
+    
             return View();
         }
 

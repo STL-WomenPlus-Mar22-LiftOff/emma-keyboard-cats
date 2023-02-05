a757023915a434740dd2eb8abc2706fd81822f5b
Brittany Davis <bddavis2011@gmail.com>
2023-02-05 16:17:46 -0600

    running into some issues, will ask in group later.

--- Areas/Identity/Data/Keyboard_CatsUser.cs
+++ Areas/Identity/Data/Keyboard_CatsUser.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
 
 namespace Keyboard_Cats.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the Keyboard_CatsUser class
 public class Keyboard_CatsUser : IdentityUser
 {  /* public string Username { get; set; }*/
-   //public string Email { get; set; }
-   //public string Password { get; set; }
+    //public string Email { get; set; }
+    //public string Password { get; set; }
+    
+}
+public class User
+{
+    public string Username { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string? firstName { get; set; }
+    public string? lastName { get; set; }
+    [Required]
+    public int Zipcode { get; set; }
+    public string? Address { get; set; }
 }
 

d4be50166d2d77fa1c97823313d8cd2058483e82
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2023-02-05 21:04:18 -0600

    small updates

--- Areas/Identity/Data/Keyboard_CatsUser.cs
+++ Areas/Identity/Data/Keyboard_CatsUser.cs
@@ -9,8 +9,7 @@ namespace Keyboard_Cats.Areas.Data;
 // Add profile data for application users by adding properties to the Keyboard_CatsUser class
 public class Keyboard_CatsUser : IdentityUser
 {
-    //public string FirstName { get; set; }
-    //public string LastName { get; set; }
+
     public string UserID { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }

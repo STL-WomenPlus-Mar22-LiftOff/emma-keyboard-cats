9555ee1ee4961ebf1f9b92f6c96d24a3f958b2ee
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2023-02-21 19:37:05 -0600

    some tweaks

--- Controllers/VetFinderController.cs
+++ Controllers/VetFinderController.cs
@@ -2,9 +2,12 @@
 using Keyboard_Cats.Models;
 using Microsoft.AspNetCore.Mvc;
 using GoogleMapsApi.Entities.Common;
-using GoogleMapsApi.Entities.PlacesFind.Request;
+using GoogleMapsApi.Entities.Places.Request;
+using GoogleMapsApi.Entities.PlaceAutocomplete.Request;
+using GooglePlacesApi.Models;
+using GoogleMapsApi.Entities.Places.Response;
 
-namespace Keyboard_Cats.Controllers
+namespace VetFinder.Controllers
 {
     public class VetFinderController : Controller
     {
@@ -12,6 +15,7 @@ namespace Keyboard_Cats.Controllers
         {
             return View();
         }
+
         public async Task<IActionResult> Search(string query)
         {
             var service = new PlacesService(new BaseClientService.Initializer
@@ -19,25 +23,63 @@ namespace Keyboard_Cats.Controllers
                 ApiKey = "AIzaSyAl4UbaqqJVgTzAHGRWwGr3sbFvVqqYQ98"
             });
 
-            var request = new PlacesSearchRequest
+            var request = new PlacesRequest
             {
                 Keyword = query,
                 Type = "veterinary_care",
-                Rankby = "distance",
+                RankBy = RankBy.Distance,
                 Location = new Location(37.7749, -122.4194) // San Francisco, CA
             };
 
-            var response = await service.Search.QueryAsync(request);
+            PlacesResponse response = await service.Search.QueryAsync(request);
 
             var viewModel = new VetFinderSearchViewModel
             {
                 Query = query,
-                Results = response.Results
+                Results = (GoogleMapsApi.Entities.PlacesFind.Response.PlacesFindResponse)response.Results
             };
 
             return View(viewModel);
         }
-
-    };
-
+    }
 }
+
+
+//namespace Keyboard_Cats.Controllers
+//{
+//    public class VetFinderController : Controller
+//    {
+//        [Route("/Index")]
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+//        public async Task<IActionResult> Search(string query)
+//        {
+//            var service = new PlacesService(new BaseClientService.Initializer
+//            {
+//                ApiKey = "AIzaSyAl4UbaqqJVgTzAHGRWwGr3sbFvVqqYQ98"
+//            });
+
+//            var request = new PlacesSearchRequest
+//            {
+//                Keyword = query,
+//                Type = "veterinary_care",
+//                Rankby = "distance",
+//                Location = new Location(37.7749, -122.4194) // San Francisco, CA
+//            };
+
+//            var response = await service.Search.QueryAsync(request);
+
+//            var viewModel = new VetFinderSearchViewModel
+//            {
+//                Query = query,
+//                Results = response.Results
+//            };
+
+//            return View(viewModel);
+//        }
+
+//    };
+
+//}
--- Models/VetFinderSearchViewModel.cs
+++ Models/VetFinderSearchViewModel.cs
@@ -1,11 +1,24 @@
 ﻿using GooglePlacesApi.Models;
+using GoogleMapsApi.Entities.PlacesDetails.Response;
+using GoogleMapsApi.Entities.PlacesFind.Response;
 
 namespace Keyboard_Cats.Models
 {
     public class VetFinderSearchViewModel
     {
         public string Query { get; set; }
-        public List<Place> Results { get; set; }
-
+        public PlacesDetailsResponse Result { get; set; }
+        public PlacesFindResponse Results { get; set; }
     }
 }
+
+
+//namespace Keyboard_Cats.Models
+//{
+//    public class VetFinderSearchViewModel
+//    {
+//        public string Query { get; set; }
+//        public List<Place> Results { get; set; }
+
+//    }
+//}

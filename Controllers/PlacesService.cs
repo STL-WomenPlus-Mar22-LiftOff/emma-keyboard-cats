d59154a4ed2f0336ef5075710f04b8545da41d73
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2023-02-25 17:23:06 -0600

    debugging placesservice

---
+++ Controllers/PlacesService.cs
@@ -0,0 +1,14 @@
+﻿using Google.Apis.Services;
+
+namespace VetFinder.Controllers
+{
+    internal class PlacesService
+    {
+        private BaseClientService.Initializer initializer;
+
+        public PlacesService(BaseClientService.Initializer initializer)
+        {
+            this.initializer = initializer;
+        }
+    }
+}
